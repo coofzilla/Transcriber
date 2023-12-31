a8192e04333e6b077ac314ea51beff1c9dc071d4
coofzilla <jeric614@yahoo.com>
2023-12-31 09:57:22 +0900

    Initial Commit

---
+++ Transcriber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transcriber
+//
+//  Created by Jeric Hernandez on 12/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transcriber/TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberApp.swift
+//  Transcriber
+//
+//  Created by Jeric Hernandez on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
